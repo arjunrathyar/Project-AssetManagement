fc1994941e3afa7515ab242baff71bfb88352ed7
DivyaRevindran <divyarevindran73@gmail.com>
2023-10-04 22:35:19 +0530

    Asset-Master edited

--- src/main/java/com/nissan/controller/AssetMasterController.java
+++ src/main/java/com/nissan/controller/AssetMasterController.java
@@ -3,17 +3,20 @@ package com.nissan.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nissan.common.APIResponse;
 import com.nissan.model.AssetMaster;
+import com.nissan.model.Vendor;
 import com.nissan.service.IAssetMasterService;
 
 @CrossOrigin
@@ -23,10 +26,13 @@ public class AssetMasterController {
 	
 
 	@Autowired
+	private APIResponse apiResponse;
+	
+	@Autowired
 	private IAssetMasterService assetMasterService;
 	
 	//get details of asset created
-	@GetMapping("/asset")
+	@GetMapping("/assets")
 	public List<AssetMaster> getNewAsset(){
 		return assetMasterService.getNewAsset();
 	}
@@ -39,9 +45,24 @@ public class AssetMasterController {
 
 	//add an asset
 	@PostMapping("/asset")
-	public void addNewAsset(@RequestBody AssetMaster assetMaster) {
+	public ResponseEntity<APIResponse> addNewAsset(@RequestBody AssetMaster assetMaster) {
+		assetMasterService.saveNewAsset(assetMaster);
+		apiResponse.setData("ASSET ADDED SUCCESSFULLY");
+		apiResponse.setStatus(200);
+
+		return ResponseEntity.status(apiResponse.getStatus()).body(apiResponse);
+
+	}
+	//Edit an asset
+	@PutMapping("/asset")
+	public ResponseEntity<APIResponse> updateEmployee(@RequestBody AssetMaster assetMaster){
+		//jwtUtil.verify(auth);
 		assetMasterService.saveNewAsset(assetMaster);
 		
+		apiResponse.setData("ASSET UPDATED SUCCESSFULLY");
+		apiResponse.setStatus(200);
+
+		return ResponseEntity.status(apiResponse.getStatus()).body(apiResponse);
 
 	}
 	
